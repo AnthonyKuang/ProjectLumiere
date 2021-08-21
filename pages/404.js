6e0ea38e6a5223d52cc2b4e48475a05801eedf12
Anthony Kuang <kuanganthony@gmail.com>
2021-08-21 11:19:22 -0700

    Set up application and added comments

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+// 404 error page (page not found)
+// This is the page that shows when a user navigates to a page that doesn't exist
+// See https://nextjs.org/docs/advanced-features/custom-error-page#404-page
+
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>;
+}
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// This augments the <html> and <body> tags of every page
+// See https://nextjs.org/docs/advanced-features/custom-document
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
