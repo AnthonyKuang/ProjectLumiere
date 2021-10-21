f622448f843f949e70753ad5063c46901cfcbb79
Arash <47273556+arashnrim@users.noreply.github.com>
2021-10-21 08:48:02 +0800

    Abstract text field style to global CSS file

--- components/ui/UsernameForm.js
+++ components/ui/UsernameForm.js
@@ -39,12 +39,12 @@ const UsernameForm = ({ initialUsername, redirectUrl }) => {
           onChange={(e) => setUsername(e.target.value)}
           value={username || initialUsername}
           placeholder="Username"
-          className="py-3 lg:py-2.5 rounded-xl border-2 bg-transparent pr-9 lg:pr-8 pl-4 text-sm border-gray-700 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 hover:border-gray-600 placeholder-gray-500 transition-colors duration-200"
+          className="text-field"
         />
         <input
           type="submit"
           value="Submit username"
-          className="px-5 py-3 lg:py-2.5 text-sm button-tertiary bg-transparent cursor-pointer"
+          className="px-5 py-3 text-sm bg-transparent cursor-pointer lg:py-2.5 button-tertiary"
           disabled={!username}
         />
       </form>
